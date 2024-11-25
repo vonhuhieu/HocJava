2387940ecf0c8bfe57f4eff92e72b8ce44484b87
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-11-25 13:58:51 +0700

    Bài 1: Cài đặt Java. Bài 2: xuất, nhập dữ liệu

---
+++ tuhocJava/src/bai1.java
@@ -0,0 +1,8 @@
+public class bai1 {
+    public static void main(String[] args) {
+        // psvm -> enter
+        // sout
+        System.out.println("xin chào mình mới học java nè");
+        System.out.println(123456);
+    }
+}
---
+++ tuhocJava/src/bai2_nhapxuatdulieu.java
@@ -0,0 +1,21 @@
+public class bai2_nhapxuatdulieu {
+    public static void main(String[] args) {
+        System.out.println("dòng 1");
+        System.out.println("dòng 2");
+        System.out.println("dòng 3");
+
+        //print
+        System.out.print("dòng 4");
+        System.out.print("dòng 5\n");
+
+        // \t
+        System.out.println("Cộng hòa xã hội chủ nghĩa Việt Nam");
+        System.out.println("\tĐộc lập - Tự do - Hạnh phúc");
+
+        // \n
+        System.out.println("Gerrard\nTorres");
+
+        // dấu nháy ""
+        System.out.println("Có người nói rằng: \"Cho đi là còn mãi\"");
+    }
+}
